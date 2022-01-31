78092f1fcda3bc8abcdbc29e821bf16e065bbd1e
shreshthgoyal <shreshthg30@gmail.com>
2022-01-31 16:12:34 +0530

    integrated OTP authentication

---
+++ src/Components/Verify/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDs3o2ZBgulsjCnv1Jv4ieMts-DQkGbC70",
+  authDomain: "quadratic-crowdfunding.firebaseapp.com",
+  projectId: "quadratic-crowdfunding",
+  storageBucket: "quadratic-crowdfunding.appspot.com",
+  messagingSenderId: "528168391756",
+  appId: "1:528168391756:web:e43ca95456d4b77d0110e9",
+  measurementId: "G-VF3CFBR3E5"
+};
+
+
+export default firebaseConfig;
