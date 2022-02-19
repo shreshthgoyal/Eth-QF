a104234d5cfc8f9940aed2b28900bc76d04c8025
Kshitij Ayush <kshitij@pop-os.localdomain>
2022-02-20 00:38:41 +0530

    reset matching function impletemented

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -6,7 +6,7 @@ import "./GetProject.sol";
 
 contract Funding is GetProject {
     uint256 public matchingFund = 0;
-    bool public isMatchingRound = true;
+    bool public isMatchingRound = false;
     mapping(address => uint256) public sponsorToDonation;
     address[] public sponsors;
 
@@ -30,6 +30,10 @@ contract Funding is GetProject {
         } else {
             sponsorToDonation[msg.sender] = msg.value;
             sponsors.push(msg.sender);
+        }
+
+        if (matchingFund > 50000000000000000000) {
+            isMatchingRound = true;
         }
     }
 
@@ -60,5 +64,12 @@ contract Funding is GetProject {
 
     function getAllSponsors() public view returns (address[] memory) {
         return sponsors;
+    }
+
+    function resetMatching(uint256 _projectId) public {
+        projects[_projectId].rootSum = 0;
+        projects[_projectId].matchingShare = 0;
+        projects[_projectId].matchingSum = 0;
+        matchingFund = 0;
     }
 }
