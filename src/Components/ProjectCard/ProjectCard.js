5093c95de7fb921c25b0bbe8a557556257c1e18f
shreshthgoyal <shreshthg30@gmail.com>
2022-02-19 20:54:10 +0530

    randomised display images

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -2,18 +2,16 @@ import { Link } from "react-router-dom";
 import "./ProjectCard.css";
 const ProjectCard = ({ project, contract, currentAccount }) => {
 
-  var images = [],
-index = 0;
-images[0] = "https://images.unsplash.com/photo-1645226027646-4113fd3b32a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
-images[1] = "https://images.unsplash.com/photo-1645226027646-4113fd3b32a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
-images[2] = "https://images.unsplash.com/photo-1645226027646-4113fd3b32a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
-index = Math.floor(Math.random() * images.length);
+var images = [];
+images[0] = "https://pbs.twimg.com/card_img/1492856766534537217/Rz9RJade?format=png&name=small";
+images[1] = "https://ethereum.org/static/810eb64d89629231aa4d8c7fe5f20ee5/7c061/developers-eth-blocks.png";
+images[2] = "https://www.pngall.com/wp-content/uploads/10/Ethereum-Logo-PNG.png";
 
   return (
     <div className="card-container">
       <div className="card">
         <div className="imgBx">
-          <img src={images[index]} alt="Project"/>
+          <img src={images[Math.floor(Math.random() * images.length)]} alt="Project"/>
         </div>
         <div className="contentBx">
           <a href={`${project.githubLink}`} alt="View repo">
