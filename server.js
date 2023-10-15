ea06d2ffb73a6040530977c36c40d4d55a643cb5
shreshthgoyal <shreshthg30@gmail.com>
2023-10-15 19:40:35 +0530

    ok

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const axios = require('axios')
 const clientID = '0661798dd8b17b1f2412'
 const clientSecret = '76c355ce64ad47d96ff2555ae90e37542a9fcba6'
 
-// Function to authorize user's Github
+// Function to authorize user's github
 
 app.get('/authenticate/:code', (req, res) => {
 
